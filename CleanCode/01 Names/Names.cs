4e388b58f41db1c8d6795ad685f5a183a0936569
Julio Velarde <cevegaju@hotmail.com>
2018-01-26 17:50:45 -0500

    Npmbres con sentido

--- CleanCode/01 Names/Names.cs
+++ CleanCode/01 Names/Names.cs
@@ -4,14 +4,14 @@ namespace CleanCode.Names
 {
     public class Names
     {
-        public Bitmap Method1(string n)
+        public Bitmap GenerateImage(string path)
         {
-            var b = new Bitmap(n);
-            var g = Graphics.FromImage(b);
-            g.DrawString("a", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 0));
-            g.DrawString("b", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 20));
-            g.DrawString("c", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 30));
-            return b;
+            var bitmap = new Bitmap(path);
+            var graphics = Graphics.FromImage(bitmap);
+            graphics.DrawString("a", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 0));
+            graphics.DrawString("b", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 20));
+            graphics.DrawString("c", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 30));
+            return bitmap;
         }
     }
 }
